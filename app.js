1fe75722fa07a617cadd07586ae3888d61d447be
Ankit Podder <ankitpodder0211@gmail.com>
2020-01-18 14:09:27 +0530

    2nd try

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
 
 
-mongoose.connect(
-  "mongodb+srv://ankit:ankit2000@cluster0-jq8ox.mongodb.net/test?retryWrites=true&w=majority"
-);
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
